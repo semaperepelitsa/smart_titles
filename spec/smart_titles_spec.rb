5d2371af31e401bbc4bd7ba695413b02c9ad6dcb
Semyon Perepelitsa <sema@sema.in>
2010-12-04 20:25:47 +0700

    adding tests

--- spec/smart_titles_spec.rb
+++ spec/smart_titles_spec.rb
@@ -1,7 +1,15 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "SmartTitles" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  include ActionView::Helpers
+  include SmartTitlesHelper
+  
+  it "#title sets up and #page_title returns the title" do
+    title 'Hello World!'
+    page_title.should == 'Hello World!'
+  end
+  
+  it "#title returns the title in h1 tag" do
+    title('Hello World!').should == '<h1>Hello World!</h1>'
   end
 end
