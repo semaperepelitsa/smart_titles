f6f1ac350f843092f59d361809388e1f02acaa73
Semyon Perepelitsa <sema@sema.in>
2011-03-14 15:56:22 +0700

    Update spec.

--- spec/smart_titles_helper_spec.rb
+++ spec/smart_titles_helper_spec.rb
@@ -1,14 +1,13 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "SmartTitles" do
+describe SmartTitles::Helper do
   include ActionView::Helpers
-  include SmartTitlesHelper
-  
+
   it "#title sets up and #page_title returns the title" do
     title 'Hello World!'
     page_title.should == 'Hello World!'
   end
-  
+
   it "#title returns the title in h1 tag" do
     title('Hello World!').should == '<h1>Hello World!</h1>'
   end
