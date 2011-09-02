3f0b81dbbd7f20351af2bc1f90de150cc5805479
Semyon Perepelitsa <sema@sema.in>
2011-09-03 00:49:09 +0400

    Covering both i18n & no-i18n versions with specs

--- spec/smart_titles_helper_spec.rb
+++ spec/smart_titles_helper_spec.rb
@@ -7,16 +7,59 @@ describe SmartTitles::Helper do
     @sample = 'Hello World!'
   end
 
-  describe '#page_title' do
-    it 'returns the title set by #title' do
-      title(@sample)
-      page_title.should == @sample
+  describe 'no i18n' do
+    describe '#page_title' do
+      it 'returns the title set by #title' do
+        title(@sample)
+        page_title.should == @sample
+      end
+    end
+
+    describe '#title' do
+      it 'returns the title in h1 tag' do
+        title(@sample).should == "<h1>#{@sample}</h1>"
+      end
     end
   end
 
-  describe '#title' do
-    it 'returns the title in h1 tag' do
-      title(@sample).should == "<h1>#{@sample}</h1>"
+  describe 'i18n' do
+    def store_translations(*args)
+      I18n.backend.store_translations(:en, *args)
+    end
+
+    before do
+      @virtual_path = 'posts/new'
+      I18n.backend.reload!
+    end
+
+    describe '#title' do
+      it "sets translated .title" do
+        store_translations posts: { new: { title: "New post" } }
+        title
+        page_title.should == "New post"
+      end
+
+      it "falls back to :title if there is no .title" do
+        store_translations title: "My Website"
+        title
+        page_title.should == "My Website"
+      end
+
+      it "sets nil title when no translated titles" do
+        title
+        page_title.should == nil
+      end
+    end
+
+    describe '#page_title' do
+      it "returns :title if title wasn't set" do
+        store_translations title: "My Website"
+        page_title.should == "My Website"
+      end
+
+      it "returns nil if title wasn't set and there is no :title" do
+        page_title.should == nil
+      end
     end
   end
 end
