1cd194622bfb045c7c7e75792098c09076c27fb7
Semyon Perepelitsa <sema@sema.in>
2011-09-03 00:24:16 +0400

    require all active_support stuff

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
-require 'active_support'
+require 'active_support/all'
 require 'action_view'
 require 'smart_titles'
 
