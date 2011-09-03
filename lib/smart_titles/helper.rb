ac3b6f19253cc7df9af5ac5081718f5a321ba525
Semyon Perepelitsa <sema@sema.in>
2011-09-03 13:02:24 +0400

    Refactoring the whole Helper. One responsabily per method.

--- lib/smart_titles/helper.rb
+++ lib/smart_titles/helper.rb
@@ -2,19 +2,28 @@ module SmartTitles
   module Helper
     MISSING_TRANSLATION = 0
 
-    def page_title(default = nil)
-      title if @page_title.nil?
-      @page_title != MISSING_TRANSLATION ? @page_title : default
+    def head_title(custom_default_title = nil)
+      page_title || custom_default_title || default_title
     end
 
-    def title(str = nil)
-      if str.nil?
-        str = t('.title', default: MISSING_TRANSLATION)
-        @page_title = t('.title', :default => [:title, MISSING_TRANSLATION])
-      else
-        @page_title = str
-      end
-      content_tag(:h1, str) unless str == MISSING_TRANSLATION
+    attr_writer :page_title
+
+    def page_title
+      @page_title || default_page_title
+    end
+
+    def title(new_title = nil)
+      self.page_title = new_title if new_title
+      content_tag(:h1, page_title) if page_title
+    end
+
+    def default_page_title
+      translation = t('.title', default: MISSING_TRANSLATION)
+      translation unless translation == MISSING_TRANSLATION
+    end
+
+    def default_title
+      t(:title)
     end
   end
 end
