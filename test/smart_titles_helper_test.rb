3dbaad5852eb23cce5a8a4616bb8cae4c0f891a3
Semyon Perepelitsa <sema@sema.in>
2011-09-05 23:51:29 +0400

    Partly move to minitest

---
+++ test/smart_titles_helper_test.rb
@@ -0,0 +1,90 @@
+require "test_helper"
+
+class SmartTitlesHelperTest < ActionView::TestCase
+  def setup
+    super
+    @av = ActionView::Base.new
+    @view_flow = ActionView::OutputFlow.new
+    @virtual_path = 'posts/new'
+    I18n.backend.reload!
+  end
+
+  def store_translations(*args)
+    I18n.backend.store_translations(:en, *args)
+  end
+
+  def page_title_translation
+    store_translations posts: { new: { title: "New post" } }
+  end
+
+  def title_translation
+    store_translations title: "My Website"
+  end
+
+  def title_translations
+    page_title_translation
+    title_translation
+  end
+
+
+  def test_page_title_with_custom_title
+    title_translations
+    title("Hi")
+    assert_equal "Hi", page_title
+  end
+
+  def test_page_title_with_translated_title
+    title_translations
+    assert_equal "New post", page_title
+  end
+
+  def test_page_title_with_no_title
+    assert_nil page_title
+  end
+
+
+  def test_custom_title
+    title_translations
+    assert_equal "<h1>Hi</h1>", title("Hi")
+  end
+
+  def test_translated_title
+    title_translations
+    assert_equal "<h1>New post</h1>", title
+  end
+
+  def test_no_page_title
+    title_translation
+    assert_nil title
+  end
+
+  def test_no_title
+    assert_nil title
+  end
+
+
+  def test_head_title_with_custom_title
+    title_translations
+    title("Hi")
+    assert_equal "Hi", head_title
+  end
+
+  def test_head_title_with_translated_titles
+    title_translations
+    assert_equal "New post", head_title
+  end
+
+  def test_head_title_with_translated_page_title
+    page_title_translation
+    assert_equal "New post", head_title
+  end
+
+  def test_head_title_with_translated_title
+    title_translation
+    assert_equal "My Website", head_title
+  end
+
+  def test_head_title_with_no_title
+    assert_includes head_title, "translation missing: en.title"
+  end
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'minitest/autorun'
+require 'active_support/all'
+require 'action_view'
+require 'smart_titles'
